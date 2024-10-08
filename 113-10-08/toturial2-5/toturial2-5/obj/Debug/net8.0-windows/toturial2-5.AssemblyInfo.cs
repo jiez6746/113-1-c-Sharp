ce450a9411ecf7512a585c842c5be113a2cff564
jiez6746 <A113221038@mail.shu.edu.tw>
2024-10-08 17:41:39 +0800

    113-10-08

--- 113-10-08/toturial2-5/toturial2-5/obj/Debug/net8.0-windows/toturial2-5.AssemblyInfo.cs
+++ 113-10-08/toturial2-5/toturial2-5/obj/Debug/net8.0-windows/toturial2-5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("toturial2-5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f93dc615df19287b2ad3d9468422e2095a5d8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36621e6f240bd704a05e4165930fc20fee22f4c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("toturial2-5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("toturial2-5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
