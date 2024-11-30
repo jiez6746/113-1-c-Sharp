8be55a125395f732fa91943bbf9568aae15f9834
jiez6746 <A113221038@mail.shu.edu.tw>
2024-11-30 15:57:38 +0800

    10-01

--- 113-10-01/PictureBoxDemo/PictureBoxDemo/obj/Debug/net8.0-windows/PictureBoxDemo.AssemblyInfo.cs
+++ 113-10-01/PictureBoxDemo/PictureBoxDemo/obj/Debug/net8.0-windows/PictureBoxDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PictureBoxDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e482a4c54a0bcbeb5bd19a102acbb9b241b7998")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8219cb18e267802c5c4370732942431e0b58f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PictureBoxDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PictureBoxDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
